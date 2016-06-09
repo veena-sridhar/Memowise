9986db363bb57c944ed8dc402cf27f930e80c426
Ben Rachbach <brachbach@gmail.com>
2016-06-09 12:47:52 -0700

    Add students to courses

--- src/server/controllers/Courses.js
+++ src/server/controllers/Courses.js
@@ -17,8 +17,7 @@ const addCourse = (req, res) => {
 };
 
 const getCourses = (req, res) => {
-  Course.find({teacherId: req.user._id}).then((courses) => {  //teacherID: '5758dd2a0fe3293550c9a40d'
-
+  Course.find({teacherId: req.user._id}).then((courses) => {  //teacherID: '5758dd2a0fe3293550c9a40d' //currently, I think it may be the case that any authorized user could maliciously add courses for any othr user by swapping the other user's object ID into their cookie; this should perhaps be improved
     res
       .status(200)
       .type('json')
@@ -26,4 +25,28 @@ const getCourses = (req, res) => {
   });
 };
 
-export default { addCourse, getCourses };
+const addStudentToCourse = (req, res) => {  
+  Course.update(
+    {_id:  req.params.courseId}, //ObjectId(
+    {$addToSet: {studentIds: req.body._id}}
+  ).then(course => {
+    res.status(201).json(course);
+  })
+  .catch(error => {
+    res
+      .status(500)
+      .type('json')
+      .json({ error });
+  });
+};
+
+const getStudentsForCourse = (req, res) => {
+  Course.find({teacherId: req.user._id}).then((courses) => {  //teacherID: '5758dd2a0fe3293550c9a40d'
+    res
+      .status(200)
+      .type('json')
+      .json(courses);
+  });
+};
+
+export default { addCourse, getCourses, addStudentToCourse, getStudentsForCourse };
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -26,6 +26,8 @@ router.route('/api/last-play/deck/:deckId').get(auth.checkAuthServer, plays.find
  */
 router.route('/api/courses').post(auth.checkAuthServer, courses.addCourse);
 router.route('/api/courses').get(auth.checkAuthServer, courses.getCourses);
+router.route('/api/courses/:courseId/students').post(courses.addStudentToCourse); //auth.checkAuthServer, 
+router.route('/api/courses/:courseId/students').get(courses.getStudentsForCourse); //auth.checkAuthServer,
 
 /*
  * Auth
