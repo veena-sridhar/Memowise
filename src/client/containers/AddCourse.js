22c8ee451ba8320b76ed6617d1be771d0a17c29f
Tegan Duong <teganduong@gmail.com>
2016-06-09 10:56:05 -0700

    Create AddCourse container

---
+++ src/client/containers/AddCourse.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { addCourse } from '../actions'
+
+let AddCourse = ({ dispatch }) => {
+  let input
+
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        dispatch(addCourse(input.value))
+        input.value = ''
+      }}>
+        <input ref={node => {
+          input = node
+        }} />
+        <button type="submit">Add Course</button>
+      </form>
+    </div>
+  )
+}
+AddCourse = connect()(AddCourse)
+
+export default AddCourse
