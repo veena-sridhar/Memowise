8c99864ec9a8c19a8819c6415eaaba8046da79bd
Veena Sridhar <v.sridhar91@gmail.com>
2016-06-09 11:13:09 -0700

    Change webpack config file to original state

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
+        loader: 'babel',
       },
       {
         test: /\.scss$/,
