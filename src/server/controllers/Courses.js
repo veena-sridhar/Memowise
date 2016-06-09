702c69659e371fa125e5aac50d0fb56968aee34e
Ben Rachbach <brachbach@gmail.com>
2016-06-09 10:02:26 -0700

    Cleanup

--- src/server/controllers/Courses.js
+++ src/server/controllers/Courses.js
@@ -3,7 +3,7 @@ import Course from '../models/Course';
 const addCourse = (req, res) => {
   return Course.create({
       courseName: req.body.courseName,
-      teacherId: req.user._id //req.user._id
+      teacherId: req.user._id //teacherID: '5758dd2a0fe3293550c9a40d'
     }).then(course => {
       const created = course.toObject();
       res.status(201).json(created);
