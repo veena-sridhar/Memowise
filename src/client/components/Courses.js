a49118cc2e60b894d6b1c32a5382746fda09a763
Tegan Duong <teganduong@gmail.com>
2016-06-09 14:27:25 -0700

    Implement getCourseInfo

--- src/client/components/Courses.js
+++ src/client/components/Courses.js
@@ -1,15 +1,17 @@
 import React, { PropTypes } from 'react';
 import AddCourse from '../containers/AddCourse';
+import { browserHistory } from 'react-router';
 
 // Presentational component that displays list of classes in teacher's view
 class Courses extends React.Component {
   constructor(props) {
     super(props);
 
+    this.getCourseInfo = this.getCourseInfo.bind(this);
   }
 
-  getCourseInfo() {
-
+  getCourseInfo(course) {
+    browserHistory.push(`/courses/${course.courseId}/students`);
   }
 
   render() {
@@ -18,7 +20,7 @@ class Courses extends React.Component {
         <AddCourse />
         <div>
           {this.props.courses.map(course =>
-            <div>{course.courseName}</div>
+            <div onClick={this.getCourseInfo}>{course.courseName}</div>
           )}
         </div>
       </div>
