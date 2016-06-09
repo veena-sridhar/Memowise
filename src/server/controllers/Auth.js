37d6d51ee5e845d5b69ef631084f225abb9c26aa
Ben Rachbach <brachbach@gmail.com>
2016-06-09 08:54:07 -0700

    Switch to final production implementation

--- src/server/controllers/Auth.js
+++ src/server/controllers/Auth.js
@@ -80,7 +80,7 @@ const checkAuthorized = (req, res) => {
 };
 
 const checkAuthServer = (req, res, next) => {
-  if (true) {   //req.user
+  if (req.user) {   
     next();
   } else {
     res.status(401).json({ message: 'not logged in' });
--- src/server/controllers/Courses.js
+++ src/server/controllers/Courses.js
@@ -3,7 +3,7 @@ import Course from '../models/Course';
 const addCourse = (req, res) => {
   return Course.create({
       name: req.body.name,
-      teacherId: '5758dd2a0fe3293550c9a40d' //req.user._id
+      teacherId: req.user._id //req.user._id
     }).then(course => {
       const created = course.toObject();
       res.status(201).json(created);
@@ -17,7 +17,7 @@ const addCourse = (req, res) => {
 };
 
 const getCourses = (req, res) => {
-  Course.find({teacherId: '5758dd2a0fe3293550c9a40d'}).then((courses) => {  //req.user._id  teacherID: '5758dd2a0fe3293550c9a40d'
+  Course.find({teacherId: req.user._id}).then((courses) => {  //teacherID: '5758dd2a0fe3293550c9a40d'
 
     console.log(courses);
 
