f8f28e8fe8b3a8371266ecd0a26368a49948ab8f
Ben Rachbach <brachbach@gmail.com>
2016-06-09 13:29:19 -0700

    Attempt to return students with nested thens

--- src/server/controllers/Courses.js
+++ src/server/controllers/Courses.js
@@ -1,4 +1,5 @@
 import Course from '../models/Course';
+import User from '../models/User';
 
 const addCourse = (req, res) => {
   return Course.create({
@@ -17,11 +18,18 @@ const addCourse = (req, res) => {
 };
 
 const getCourses = (req, res) => {
-  Course.find({teacherId: req.user._id}).then((courses) => {  //teacherID: '5758dd2a0fe3293550c9a40d' //currently, I think it may be the case that any authorized user could maliciously add courses for any othr user by swapping the other user's object ID into their cookie; this should perhaps be improved
+  Course.find({teacherId: req.user._id})
+  .then((courses) => {  //teacherID: '5758dd2a0fe3293550c9a40d' //currently, I think it may be the case that any authorized user could maliciously add courses for any othr user by swapping the other user's object ID into their cookie; this should perhaps be improved
     res
       .status(200)
       .type('json')
       .json(courses);
+  })
+  .catch(error => {
+    res
+      .status(500)
+      .type('json')
+      .json({ error });
   });
 };
 
@@ -41,12 +49,31 @@ const addStudentToCourse = (req, res) => {
 };
 
 const getStudentsForCourse = (req, res) => {
-  Course.find({teacherId: req.user._id}).then((courses) => {  //teacherID: '5758dd2a0fe3293550c9a40d'
+  Course.findOne({_id: req.params.courseId})
+  .then(course => {
+    console.log(course);
+    // studentsToFind = course.studentIds.map()
+    User.find(
+      {_id: { $in: [course.studentIds] }}
+    )
+    .then(students => {
+      console.log(students);
+      course.students = students;
+      res
+        .status(200)
+        .type('json')
+        .json(course);
+    });
+  })
+  .catch(error => {
     res
-      .status(200)
+      .status(500)
       .type('json')
-      .json(courses);
+      .json({ error });
   });
 };
+
+//will need to get course for both students and for decks, so that should be its own thing
+//can chain on the appropriate then 
 
 export default { addCourse, getCourses, addStudentToCourse, getStudentsForCourse };
