aad969531cbcca395c07130dabe9bf07607b7d04
Tegan Duong <teganduong@gmail.com>
2016-06-10 16:25:20 -0700

    Comment out code for testing

--- src/client/containers/Students.js
+++ src/client/containers/Students.js
@@ -1,6 +1,34 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import StudentsTab from '../components/StudentsTab';
+import { fetchStudents, selectCourse } from '../actions';
+
+// class Students extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   componentDidMount() {
+//     const { dispatch, selectedCourse } = this.props
+//     dispatch(fetchStudentsIfNeeded(selectedCourse))
+//   }
+
+//   componentWillReceiveProps(nextProps) {
+//     if (nextProps.selectedCourse !== this.props.selectedCourse) {
+//       const { dispatch, selectedCourse } = nextProps
+//       dispatch(fetchStudentsIfNeeded(selectedCourse))
+//     }
+//   }
+
+//   handleChange(nextCourse) {
+//     this.props.dispatch(selectCourse(nextCourse))
+//   }
+// }
 
 const mapStateToProps = ({ students }) => ({ students });
+const mapDispatchToProps = (dispatch) => ({
+  fetchStudents: (courseId) => dispatch(fetchStudents(courseId)),
+  selectCourse: (course) => dispatch(selectCourse(course))
+});
 
-export default connect(mapStateToProps)(StudentsTab);
+export default connect(mapStateToProps, mapDispatchToProps)(StudentsTab);
