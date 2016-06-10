61ecf855c1249bf75edcc59f8e73b63fcb738263
Veena Sridhar <v.sridhar91@gmail.com>
2016-06-10 11:37:32 -0700

    Add map function to sort cards

--- src/client/components/Decks.js
+++ src/client/components/Decks.js
@@ -1,17 +1,16 @@
 import React, { PropTypes } from 'react';
 import _ from 'lodash';
-import $ from 'jquery';
 import DeckItem from './DeckItem';
 
 const Decks = ({ decks }) => (
   <div className="container">
-    <h4 className="center grey-text text-darken-4"> Decks</h4>
+    <h4 className="center grey-text text-darken-4">Decks</h4>
     <div className="card-list">
       <div className="card-columns">
-        {_.map(decks, (deckSet, index) => {
-            <h5>{index}</h5>
-            _.map(deckSet, (deck, index) => {<DeckItem key={index} deck={deck} />});
-          })
+        {_.map(decks, function (deckSet, index) {
+            return <h5>{index}</h5>
+            _.map(deckSet, function (deck, index) { return <DeckItem key={index} deck={deck} /> }
+          )})
         }
       </div>
     </div>
