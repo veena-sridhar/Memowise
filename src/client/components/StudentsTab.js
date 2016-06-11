9878855b92d03de07ec103e2a6ab013e4d51a749
Tegan Duong <teganduong@gmail.com>
2016-06-10 18:46:13 -0700

    Clean up code and remove comments

--- src/client/components/StudentsTab.js
+++ src/client/components/StudentsTab.js
@@ -5,95 +5,40 @@ import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import { receiveStudents, failedRequest } from '../actions'
 
-// const mapDispatchToState = (dispatch) => ({
-//   setCourseState: (course) => dispatch(selectCourse(course))
-// });
+const mapStateToProps = ({ students }) => ({ students });
 
 class StudentsTab extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      course: {}
-    }
   }
 
   componentDidMount() {
-    fetch(`/api/courses/${this.props.params.courseId}/students`, {
+    return fetch(`/api/courses/${this.props.params.courseId}/students`, {
       credentials: 'same-origin'
     })
     .then(res => res.json())
-    // .then(students => {
-    //   this.setState({course: students})
-    // })
     .then(students => dispatch(receiveStudents(students)))
     .catch(err => dispatch(failedRequest(err)))
   }
 
-  // componentDidMount() {
-  //   const { dispatch, selectedCourse } = this.props
-  //   dispatch(fetchStudentsIfNeeded(selectedCourse))
-  // }
-
-  // componentWillReceiveProps(nextProps) {
-    
-    
-  //   this.props.students = nextProps;
-    
-  // }
-
-  // componentDidMount() {
-  //   this.props.fetchStudents(this.props.params.courseId)
-  //   .then(course => {
-  //     console.log('course in StudentsTab: ', course);
-  //     this.setState({course: course.data});
-  //   })
-  // }
-
   render() {
-    console.log('this.props.course: ', this.props.course);
-    console.log('this.props.students: ', this.props.students);
     return (
       <div className="container">
         <h1>
-          
+          {this.props.students.courseName}
         </h1>
         <AddStudent courseId={this.props.params.courseId} />
         <div>
-          {this.props.students.map(student =>
-            <span>
-              <div>{student.name}</div>
-              <div>{student.email}</div>
-            </span>
+          {this.props.students.students.map(student =>
+            <div>
+              <span>{student.name} {student.email}</span>
+            </div>
           )}
         </div>
       </div>
     );
   }
-
-  // render() {
-  //   console.log('this.props.course: ', this.props.course);
-  //   console.log('this.props.students: ', this.props.students);
-  //   return (
-  //     <div className="container">
-  //       <AddStudent courseId={this.props.params.courseId} />
-  //       <div>
-  //         {this.props.students.map(student =>
-  //           <span>
-  //             <div>{student.name}</div>
-  //             <div>{student.email}</div>
-  //           </span>
-  //         )}
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
 };
 
-// StudentsTab.propTypes = {
-//   students: PropTypes.array.isRequired,
-// };
-
-export default StudentsTab;
-// export default connect(null, mapDispatchToState)(StudentsTab);
+export default connect(mapStateToProps)(StudentsTab);
