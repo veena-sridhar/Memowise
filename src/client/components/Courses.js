a90c6929b61090736ed95db85e1b8c70b0d5a738
Tegan Duong <teganduong@gmail.com>
2016-06-10 19:05:02 -0700

    Change Courses into a stateless functional component

--- src/client/components/Courses.js
+++ src/client/components/Courses.js
@@ -1,39 +1,21 @@
 import React, { PropTypes } from 'react';
 import AddCourse from '../containers/AddCourse';
-import { browserHistory } from 'react-router';
 import CourseInfo from './CourseInfo';
 
 // Presentational component that displays list of classes in teacher's view
-class Courses extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // this.handleCourseClick = this.handleCourseClick.bind(this);
-  }
-
-  // handleCourseClick(course) {
-  //   console.log('course >>>>', course);
-  //   this.props.dispatch(selectCourse(course))
-  //   // this.props.onCourseClick(this.props.course);
-  //   // this.props.fetchStudents(course._id);
-  //   browserHistory.push(`/courses/${course._id}/students`);
-  // }
-
-  render() {
-    return (
-      <div className="container">
-        <AddCourse />
-        <div>
-          {this.props.courses.map(course =>
-            <CourseInfo 
-            course={course}
-            />
-          )}
-        </div>
-      </div>
-    );
-  }
-}
+const Courses = ({ courses }) => (
+  <div className="container">
+    <AddCourse />
+    <div>
+      {courses.map(course =>
+        <CourseInfo 
+        key={course._id}
+        course={course}
+        />
+      )}
+    </div>
+  </div>
+);
 
 Courses.propTypes = {
   courses: PropTypes.array.isRequired,
